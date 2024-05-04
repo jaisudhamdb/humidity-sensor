c81299739ab02f3eb5efad5c8c3cb8d376568487
j0b0n6m <jaisudha.baskaran@walmart.com>
2024-05-04 16:13:50 +0530

    Refactored

--- src/main/scala/SensorHumidity.scala
+++ src/main/scala/SensorHumidity.scala
@@ -1,3 +1,28 @@
+import java.io.File
+
 object SensorHumidity extends App {
-println("hello")
+
+  var fileList: List[File] = null
+  val directory = "src/main/resources/sensor/humidity"
+
+
+  val filesProcessed = noOfProcessedFiles(directory)
+  println(s"Number of files processed = $filesProcessed" )
+
+  def noOfProcessedFiles(d :String) = {
+    val dir = new File(d)
+    if (dir.exists && dir.isDirectory) {
+      fileList = dir.listFiles.filter(_.isFile).toList
+    } else {
+      List[File]()
+    }
+     fileList.size
+  }
+
+  def numOfProcessedMeasurements(): Int={
+    var conf = new SparkConf().setAppName("Read CSV Files From Directory").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+    val sqlContext = new SQLContext(sc)
+
+
 }
