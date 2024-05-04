8bdbce3d44cba7b1ba01cb318a5040529e1e5e60
j0b0n6m <jaisudha.baskaran@walmart.com>
2024-05-04 16:28:41 +0530

    Added logic for 3 outputs

--- src/main/scala/SensorHumidity.scala
+++ src/main/scala/SensorHumidity.scala
@@ -4,7 +4,8 @@ object SensorHumidity extends App {
 
   var fileList: List[File] = null
   val directory = "src/main/resources/sensor/humidity"
-
+  var humidityListBuffer = scala.collection.mutable.ListBuffer[String]
+  var sensorIdListBuffer = scala.collection.mutable.ListBuffer[String]
 
   val filesProcessed = noOfProcessedFiles(directory)
   println(s"Number of files processed = $filesProcessed" )
@@ -19,10 +20,39 @@ object SensorHumidity extends App {
      fileList.size
   }
 
-  def numOfProcessedMeasurements(): Int={
+  def numOfProcessedMeasurements(): Int= {
     var conf = new SparkConf().setAppName("Read CSV Files From Directory").setMaster("local[*]")
     val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
+
+
+    for (file <- fileList) {
+      val textRDD = sc.textFile(file.toString)
+      val header = textRDD.first()
+      val textRDDResult = textRDD.filter(row => row != header)
+      val empRdd = textRDDResult.map {
+        line =>
+          val col = line.split(",")
+          var humidityList = col(0)
+          var sensorIdList = col(1)
+          (humidityList, sensorIdList)
+      }
+    }
+  }
+
+  def numOfFailedMeasurements():Int={
+    val HumidityListwithNaNData : List[String]=humidityListBuffer.toList.flatten
+    var count=0
+    for (i<- 0 until (humidityListBuffer.toList.flatten.length))
+    {
+      if(HumidityListwithNaNData(i).equals("NaN") )
+      {
+        count=count+1
+      }
+    }
+    count
+  }
+
 
 
 }
